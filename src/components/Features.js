18f4e89e161be39472f4af9bf0f2a2ef2d3e420c
Banigo Kenebebh <kenebebhbanigo@gmail.com>
2022-10-05 05:15:37 +0100

    just a little work on the features

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,11 +1,26 @@
 import * as React from "react";
+import icon from "/public/static/banknote.svg";
 
 const Features = () => {
+  const feats = [
+    { title: "firstItem", id: 1, imgSrc: icon },
+    { title: "secondItem", id: 2, imgSrc: icon },
+  ];
+
   return (
     <div className="featuresContainer py-8">
       <h1 className="flex justify-center uppercase text-4xl text-green-600 pb-8 font-bold">
         Our main features
       </h1>
+
+      <div className="featss">
+        {feats.map((feat) => (
+          <div className="parent">
+            <img src={feat.imgSrc} />
+            <h2>{feat.title}</h2>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
