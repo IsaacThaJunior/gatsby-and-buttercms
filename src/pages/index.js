f8552d1aca4a436719dd8c07adcc5923edcf68db
IsaacThaJunior <isaacjunez669@gmail.com>
2022-10-08 10:23:40 +0100

    okay

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,39 @@ import * as React from 'react';
 import axios from 'axios';
 import Bigone from '../components/Bigone';
 
-// const URL = `https://api.buttercms.com/v2/posts/example-post?auth_token=${process.env.BUTTER_CMS_API_KEY}`;
+const URL = `https://api.buttercms.com/v2/posts/example-post?auth_token=${process.env.BUTTER_CMS_API_KEY}`;
+
+
 const IndexPage = () => {
+	React.useEffect(() => {
+		axios
+			.get(URL)
+			.then((res) => {
+				console.log(res.data.data);
+			})
+			.catch((err) => console.log(err));
+	}, []);
 
-	return <main>
+	return (
+		<main>
+			<h1 className="text-3xl font-bold bg-red-700 underline">
+				Hey from IndexPage
+			</h1>
 
-    <h1 className="text-3xl font-bold bg-red-700 underline">Hey from IndexPage</h1>
+			<h3>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore ipsa
+				quasi modi asperiores eos. Accusantium excepturi rerum quia sint
+				molestiae reprehenderit tempore! Dolor rerum exercitationem sequi animi
+				quod ducimus veritatis quia a adipisci. Delectus repudiandae corporis
+				consectetur culpa accusantium neque dignissimos nemo, sed sunt amet
+				repellat. Tempore consequatur quos aperiam!
+			</h3>
+			<hr />
 
-    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore ipsa quasi modi asperiores eos. Accusantium excepturi rerum quia sint molestiae reprehenderit tempore! Dolor rerum exercitationem sequi animi quod ducimus veritatis quia a adipisci. Delectus repudiandae corporis consectetur culpa accusantium neque dignissimos nemo, sed sunt amet repellat. Tempore consequatur quos aperiam!</h3>
-    <hr />
-
-    <Bigone />
-    <hr />
-  </main>;
+			<Bigone />
+			<hr />
+		</main>
+	);
 };
 
 export default IndexPage;
